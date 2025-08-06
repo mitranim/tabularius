e2fb554196acbbb2c3576c131bf0c5e70b32ac68
Nelo Mitranim <me@mitranim.com>
2025-08-06 08:26:09 +0200

    fix `/api/db`

--- server/api/api_db.mjs
+++ server/api/api_db.mjs
@@ -31,5 +31,7 @@ export async function apiDb(ctx) {
   try {await (await ctx.conn()).run(`checkpoint`)}
   catch (err) {console.error(`[api_db] unable to checkpoint:`, err)}
 
-  return (u.HttpFile.resolve(path)).response()
+  const file = await u.HttpFile.resolve(path)
+  if (!file) throw Error(`internal: missing DB file`)
+  return file.response()
 }
